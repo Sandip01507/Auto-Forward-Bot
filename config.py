b489e5fded8108192e5c46938d11743a838a2ea8
S∆NDiP <sandip01507@gmail.com>
2024-12-11 13:28:09 +0530

    DONE

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import getenv
 
 class Config(object):
-      API_HASH = getenv("API_HASH", "abcd")
-      API_ID = int(getenv("API_ID", 1234))      
-      BOT_TOKEN = getenv("BOT_TOKEN", "1234:abcd")
-      CHANNEL = list(x for x in getenv("CHANNEL", "-100123:-100456").replace("\n", " ").split(' '))
+      API_HASH = getenv("API_HASH", "c7e952440251e33bb5cce566b29f7254")
+      API_ID = int(getenv("API_ID", 18029060))      
+      BOT_TOKEN = getenv("BOT_TOKEN", "7592982888:AAEfg-bVArXtC07OwjOKQX4ipPqV9TTtE2Y")
+      CHANNEL = list(x for x in getenv("CHANNEL", "-1001815869253:-1002317285080").replace("\n", " ").split(' '))
